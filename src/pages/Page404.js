0f53205a6d868187b2b7eaba4386d6ac418ce9ba
sagarscalelot <sagarkheni.scalelot@gmail.com>
2022-11-07 17:15:31 +0530

    update

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Page404 = () => {
-  return <>
-     <div>
-        <img src="https://cdn.dribbble.com/users/1138875/screenshots/4669703/404_animation.gif" alt="" />
-        <NavLink to="/app"><button className='btn btn-primary'>Go to home Page</button></NavLink>
-
-
-    </div>
-  </>;
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection:"column"
+        }}
+      >
+        <div>
+          <img
+            src="https://cdn.dribbble.com/users/1138875/screenshots/4669703/404_animation.gif"
+            alt=""
+          />
+        </div>
+        <div>
+          <Link to="/app">
+            <button
+              className="btn btn-primary"
+              style={{
+                padding: "10px",
+                backGroungColor: "violet",
+                fontSize: "18px",
+                color: "Red",
+              }}
+            >
+              Go to home Page
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
-
 
 export default Page404;
