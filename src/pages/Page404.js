aa086c3d27ff86510976a99e32b03e594843c730
sagarscalelot <sagarkheni.scalelot@gmail.com>
2022-11-07 17:06:13 +0530

    update

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 const Page404 = () => {
-  return <>404</>;
+  return <>
+     <div>
+        <img src="https://cdn.dribbble.com/users/1138875/screenshots/4669703/404_animation.gif" alt="" />
+        <NavLink to="/app"><button className='btn btn-primary'>Go to home Page</button></NavLink>
+
+
+    </div>
+  </>;
 };
 
 
